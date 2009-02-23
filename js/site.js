275cd3084976365385f5fc07544fe57e4977668d
Mike Lundy <mike@fluffypenguin.org>
2009-02-23 03:14:20 -0700

    load repos from github

--- js/site.js
+++ js/site.js
@@ -10,3 +10,34 @@ function pickColor(name)
             elt.disabled = false;
     }
 }
+
+function sortKey(key) {
+    return function(a,b) {
+        return (a[key] > b[key]) - (a[key] < b[key]);
+    }
+}
+
+// description -> tooltip
+//
+//"owner": "novas0x2a",
+//"forks": 0,
+//"name": "novas0x2a.github.com",
+//"private": false,
+//"url": "http://github.com/novas0x2a/novas0x2a.github.com",
+//"watchers": 1,
+//"fork": false,
+//"homepage": "novas0x2a.github.com"
+function populateCode(json) {
+    var repo = json.user.repositories.sort(sortKey('name'));
+
+    jQuery(function() {
+        jQuery.each(repo, function(i, val) {
+            if (val.private)
+                return;
+            $("#code_list").append(
+                "<li><a title=\"" + val.description + "\" href=\"" + val.url + "\">" +
+                     val.name.replace(".github.com", "") +
+                "</a></li>");
+        });
+    });
+}
