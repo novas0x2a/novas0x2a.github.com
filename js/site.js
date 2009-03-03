dbe2ad4b9c20c036a5cb449816b2a788ba84ed09
Mike Lundy <mike@fluffypenguin.org>
2009-03-03 11:22:27 -0800

    use reset css, layouts, and getJSON

--- js/site.js
+++ js/site.js
@@ -11,11 +11,21 @@ function pickColor(name)
     }
 }
 
-function sortKey(key) {
+function sortAttr(key) {
     return function(a,b) {
         return (a[key] > b[key]) - (a[key] < b[key]);
     }
 }
+
+function sortKey(f) {
+    return function(a,b) {
+        var at = f(a);
+        var bt = f(b);
+        return (at > bt) - (at < bt);
+    }
+}
+
+var sortText = sortKey(function(a) {return $(a).text();})
 
 // description -> tooltip
 //
@@ -27,17 +37,26 @@ function sortKey(key) {
 //"watchers": 1,
 //"fork": false,
 //"homepage": "novas0x2a.github.com"
-function populateCode(json) {
-    var repo = json.user.repositories.sort(sortKey('name'));
+// <script src="http://github.com/api/v1/json/novas0x2a?callback=populateCode" type="text/javascript"></script>
 
-    jQuery(function() {
-        jQuery.each(repo, function(i, val) {
-            if (val.private)
-                return;
-            $("#code_list").append(
-                "<li><a title=\"" + val.description + "\" href=\"" + val.url + "\">" +
-                     val.name.replace(".github.com", "") +
-                "</a></li>");
-        });
-    });
-}
+$(function() {
+    $.getJSON('http://github.com/api/v1/json/novas0x2a?callback=?',
+        function(json) {
+            var repo = json.user.repositories.sort(sortAttr('name'));
+
+            $(repo).each(function(i, val) {
+                if (val.private)
+                    return;
+                $("#code_list").append(
+                    "<li><a title=\"" + val.description + "\" href=\"" + val.url + "\">" +
+                         val.name.replace(".github.com", "") +
+                    "</a></li>");
+            });
+
+            // sort it afterward to get the entries in the html, too
+            var $kids = $("#code_list").children();
+            var $sorted = jQuery.makeArray($kids).sort(sortText);
+            $($sorted).appendTo("#code_list");
+        }
+    )
+});
