5ab971ebe2ae62997037a9d554d589edd89256d9
Mike Lundy <mike@fluffypenguin.org>
2009-03-04 14:42:43 -0800

    css colors are dynamicish, and a better pickColor

--- js/site.js
+++ js/site.js
@@ -1,14 +1,6 @@
 function pickColor(name)
 {
-    var elts = $('link');
-    for (var i = 0, len=elts.length; i < len; ++i)
-    {
-        var elt = elts[i];
-        if (elt.rel && elt.title && elt.rel.indexOf("stylesheet") != -1)
-            elt.disabled = true;
-        if (elt.title == name)
-            elt.disabled = false;
-    }
+    $('link[title][rel*=stylesheet]').attr('disabled', true).filter('[title=' + name + ']').attr('disabled', false);
 }
 
 function sortAttr(key) {
